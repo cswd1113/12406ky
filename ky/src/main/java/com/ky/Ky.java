0a6157322e4cffb8b12ba83b266da6f701a32654
cswd1113 <1013653754@qq.com>
2015-03-18 16:33:11 +0800

    Ky.java
    Ky类创建

---
+++ ky/src/main/java/com/ky/Ky.java
@@ -0,0 +1,18 @@
+package com.ky;
+
+/**  
+ * @Title: Ky.java
+ * @Package com.ky
+ * @Description: 
+ * @author guokp
+ * @date 2015-3-18 下午04:31:46
+ * @Copyright: 12406  2015 
+ * @version V1.0  
+ */
+public class Ky {
+
+	public static void main(String[] args) {
+		System.out.println("欢迎来到快阅");
+	}
+
+}
